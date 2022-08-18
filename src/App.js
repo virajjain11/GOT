f3c48541d47b05e98bce0da6b94def7c0615c65f
Viraj <virajjain7.vj@gmail.com>
2022-08-18 12:23:21 +0530

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Home from "./components/HomePage/Home";
-import { Provider } from "react-redux";
-import { store } from "./features/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CardDetails from "./components/Card.js/CardDetails/CardDetails";
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Home />
-      </Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" exact element={<Home />} />
+          <Route path="/:name/:id" element={<CardDetails />} />
+          {/* <Home /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
