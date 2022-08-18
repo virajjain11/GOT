7b4739c5c96e85189269e4cd02d49adf7d81ff16
Viraj <virajjain7.vj@gmail.com>
2022-08-18 14:07:47 +0530

    code refactor

--- src/components/Card.js/CardDetails/CardDetails.js
+++ src/components/Card.js/CardDetails/CardDetails.js
@@ -12,37 +12,35 @@ const CardDetails = () => {
     dispatch(fetchItems({ type: name, id: id }));
   }, []);
   // console.log(data);
+  let obj;
+  if (Object.keys(data).length !== 0) {
+    obj = {
+      Authors: data.authors.join(", "),
+      Country: data.country,
+      "Total characters": data.characters.length,
+      isbn: data.isbn,
+      "Number of Pages": data.numberOfPages,
+      "Media Type": data.mediaType,
+      "Released date": new Date(data.released).toDateString(),
+    };
+  }
   return (
     <>
-      <div>
-        {Object.keys(data).length !== 0 && (
-          <>
-            <h1 className="font-semibold text-lg ">{data.name}</h1>
-            <p> Authors: {data.authors.join(", ")}</p>
-            <p> country: {data.country}</p>
-            <p> Total characters :{data.characters.length}</p>
-            <p> isbn :{data.isbn}</p>
-            <p> numberOfPages :{data.numberOfPages}</p>
-            <p> publisher: {data.publisher}</p>
-            <p> mediaType: {data.mediaType}</p>
-            <p> released date: {new Date(data.released).toDateString()}</p>
-          </>
-        )}
-      </div>
+      {/* <div> */}
+      {Object.keys(data).length !== 0 && (
+        <div className="bg-slate-300  w-[300px] md:w-[600px]  m-[2%] pb-4">
+          <h1 className="font-semibold text-xl tracking-wider text-center pt-4">
+            {data.name}
+          </h1>
+          {Object.entries(obj).map((array, idx) => (
+            <p key={idx} className="pl-2 pb-2">
+              {array[0]} :{array[1]}
+            </p>
+          ))}
+        </div>
+      )}
     </>
   );
 };
 
 export default CardDetails;
-
-{
-  /* <h1 className="font-semibold text-lg ">{data.name}</h1>
-        <p> Authors: {authors}</p>
-        <p> country: {data.country}</p>
-        <p> Total characters :{data.characters.length}</p>
-        <p> isbn :{data.isbn}</p>
-        <p> numberOfPages :{data.numberOfPages}</p>
-        <p> publisher: {data.publisher}</p>
-        <p> mediaType: {data.mediaType}</p>
-        <p> released date: {date}</p> */
-}
