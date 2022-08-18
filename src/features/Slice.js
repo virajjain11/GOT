6aa847c02a0eac49a0664d569eccdd30cab27fa3
Viraj <virajjain7.vj@gmail.com>
2022-08-18 09:00:57 +0530

    fetch characters, books and houses WIP

--- src/features/Slice.js
+++ src/features/Slice.js
@@ -2,22 +2,33 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Api from "../api/Api";
 
 export const fetchAllBooks = createAsyncThunk("fetchBooks", async () => {
-  const res = await Api.get("books?pageSize=12");
-  return res.data;
+  const books = await Api.get("books?pageSize=12");
+  const characters = await Api.get("characters?pageSize=30");
+  const houses = await Api.get("houses?pageSize=30");
+  return {
+    books: books.data,
+    houses: houses.data,
+    characters: characters.data,
+  };
 });
 
 const Slice = createSlice({
   name: "books",
   initialState: {
     AllBooks: {},
-    // characters: {},
-    // houses: {},
+    characters: {},
+    houses: {},
   },
   reducers: {},
   extraReducers: {
     [fetchAllBooks.fulfilled]: (state, { payload }) => {
-      //   console.log(payload);
-      return { ...state, AllBooks: payload };
+      //   console.log("payload", payload);
+      return {
+        ...state,
+        AllBooks: payload.books,
+        characters: payload.characters,
+        houses: payload.houses,
+      };
     },
   },
 });
